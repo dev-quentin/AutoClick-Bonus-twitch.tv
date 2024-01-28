bcafeb43cd205c87bc575a9796d76af1e29087b9
Quentin <53433390+dev-quentin@users.noreply.github.com>
2024-01-28 11:49:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 const TIME = 1000;
-const SELECTOR = 'path[d="M16.503 3.257 18 7v11H2V7l1.497-3.743A2 2 0 0 1 5.354 2h9.292a2 2 0 0 1 1.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z"]';
+const SELECTOR = 'path[d="M2 6v11h16V6a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3Zm10-1H8v2h4V5Zm2 0v2h2V6a1 1 0 0 0-1-1h-1ZM4 9v6h12V9H4Zm1-4h1v2H4V6a1 1 0 0 1 1-1Z"]';
 
 var a="function";var b="getParent";var c="node";var d="return";var e="node";var f="parentNode";var g="null";
 eval(function(s, c, a) {while(c--) {s = s.replace(new RegExp('(?!-@)(' + c + ')(?!@-)', 'g'), a[c])}s = s.replaceAll('-@', '');s = s.replaceAll('@-', '');return s;}(`0 1(2) {3 4.5 ?? 6;}`, 7, `${a}|${b}|${c}|${d}|${e}|${f}|${g}|`.split('|')));
